9958fc79e3564d1d3d62b3c50d6eeaaaceb5dbaa
mohammed-ashiq-m <ashiqmvkpadi@gmail.com>
2019-09-16 14:07:14 +0530

    event loop

--- main.js
+++ main.js
@@ -10,8 +10,29 @@ http.createServer(function (req,res) {
 
 console.log("server is successfully running at http://127.0.0.1:8081/ ")//server status at the run time
 
+// C A L L _ B A C K _ C O N C E P T//
 
 var fs=require("fs")
 var data=fs.readFileSync('input.txt')
 console.log(data.toString());
+console.log('program ended');
+
+//E V E N T _ L O O P//
+
+var events=require("events")
+var eventEmitter=new events.EventEmitter();
+
+connectHandler=function connected() {
+    console.log('connected Succcessfully');
+    eventEmitter.emit('data_received')
+}
+
+
+eventEmitter.on('connection',connectHandler);
+
+eventEmitter.on("data_received",function (){
+    console.log('datad received successfully');
+
+});
+eventEmitter.emit('connection');
 console.log('program ended');
